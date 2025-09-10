94cc03af0536e23299bfbdb3e022b4d1feeca3b8
Smiley98 <smileysofficialcinema@gmail.com>
2025-09-10 15:18:17 -0400

    Week 1 solution

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -5,23 +5,25 @@ int main()
 {
     InitWindow(800, 800, "Physics-1");
 
-    Vector2 circle_position_1 = { 400.0f, 400.0f };
-    Vector2 circle_position_2 = { 400.0f, 400.0f };
+    Vector2 circle_position = { 400.0f, 400.0f };
     float circle_radius = 25.0f;
     Color circle_color = PURPLE;
+
+    float a = 1.0f;
+    float b = 50.0f;
 
     while (!WindowShouldClose())
     {
         float t = GetTime();
         float dt = GetFrameTime();
-        circle_position_1.x = 400.0f + 150.0f * sinf(t);    // moves left/right within a range of 150 units forever
-        circle_position_2.y += 10.0f * dt;                  // moves down at 10 units per second
+
+        circle_position.x = circle_position.x + (-sinf(t * a)) * a * b * dt;
+        circle_position.y = circle_position.y + (cosf(t * a)) * a * b * dt;
 
         BeginDrawing();
             ClearBackground(WHITE);
-            DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
-            DrawCircleV(circle_position_1, circle_radius, circle_color);
-            DrawCircleV(circle_position_2, circle_radius, circle_color);
+            DrawText(TextFormat("Total Time: %f", t), 750, 0, 20, BLUE);
+            DrawCircleV(circle_position, circle_radius, circle_color);
         EndDrawing();
     }
 
